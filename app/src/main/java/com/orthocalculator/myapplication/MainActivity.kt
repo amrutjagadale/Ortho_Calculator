8cdf852f6071de83452d02150bce00944c5b4d1b
Radhika <bajajrids@gmail.com>
2025-05-21 13:09:56 +0530

    UI changes

--- app/src/main/java/com/orthocalculator/myapplication/MainActivity.kt
+++ app/src/main/java/com/orthocalculator/myapplication/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.orthocalculator.myapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bottomNav: BottomNavigationView
     private val homeUrl = "https://www.orthocalculator.org/li4c7hto7ovrpmttreft5c23bgi1vc"
 
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,7 +36,19 @@ class MainActivity : AppCompatActivity() {
 
         // BottomNav
         bottomNav = findViewById(R.id.bottom_navigation)
+
         bottomNav.setOnItemSelectedListener { item ->
+            val menuView = bottomNav.getChildAt(0) as BottomNavigationMenuView
+            for (i in 0 until menuView.childCount) {
+                val itemView = menuView.getChildAt(i)
+                val menuItem = bottomNav.menu.getItem(i)
+                if (menuItem.itemId == item.itemId) {
+                    itemView.setBackgroundResource(R.color.nav_item_selected_bg) // Selected background
+                } else {
+                    itemView.setBackgroundResource(android.R.color.transparent) // Unselected background
+                }
+            }
+
             when (item.itemId) {
                 R.id.nav_back -> {
                     if (myWebView.canGoBack()) myWebView.goBack() else finish()
