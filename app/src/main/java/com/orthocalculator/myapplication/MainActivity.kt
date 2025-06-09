b00d8525747a3aacc60c386a0962acf6e4cbf097
Radhika <bajajrids@gmail.com>
2025-06-09 16:21:06 +0530

    UI changes

--- app/src/main/java/com/orthocalculator/myapplication/MainActivity.kt
+++ app/src/main/java/com/orthocalculator/myapplication/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.orthocalculator.myapplication
 
+import android.content.Context
 import android.graphics.PorterDuff
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.MenuItem
 import android.view.MotionEvent
 import android.view.View
 import android.webkit.WebView
@@ -12,6 +16,7 @@ import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
@@ -19,7 +24,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var myWebView: WebView
     private lateinit var bottomNavLayout: LinearLayout
-    private val homeUrl = "https://www.orthocalculator.org/!4c7hto7ovrpmtref5c23bgi1vc"
+    private val homeUrl = "https://www.orthocalculator.org/li4c7hto7ovrpmttreft5c23bgi1vc"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,48 +61,44 @@ class MainActivity : AppCompatActivity() {
             PorterDuff.Mode.SRC_IN
         )
 
-        // Set touch listener for press effect
-        button.setOnTouchListener { v, event ->
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    // Highlight on press
-                    button.setColorFilter(
-                        ContextCompat.getColor(this, R.color.highlight),
-                        PorterDuff.Mode.SRC_IN
-                    )
-                    v.isPressed = true
-                    true
-                }
+        button.setOnClickListener {
+            // Highlight the button
+            button.setColorFilter(
+                ContextCompat.getColor(this, R.color.highlight),
+                PorterDuff.Mode.SRC_IN
+            )
 
-                MotionEvent.ACTION_UP -> {
-                    button.setColorFilter(
-                        ContextCompat.getColor(this, R.color.white),
-                        PorterDuff.Mode.SRC_IN
-                    )
-                    v.isPressed = false
+            // Revert after 2 seconds
+            Handler(Looper.getMainLooper()).postDelayed({
+                button.setColorFilter(
+                    ContextCompat.getColor(this, R.color.white),
+                    PorterDuff.Mode.SRC_IN
+                )
+            }, 2000)
 
-                    // Handle navigation
-                    when (v.id) {
-                        R.id.nav_back -> if (myWebView.canGoBack()) myWebView.goBack() else finish()
-                        R.id.nav_home -> myWebView.loadUrl(homeUrl)
-                        R.id.nav_tutorial -> myWebView.loadUrl("https://www.orthocalculator.org/app-video-library")
-                    }
-                    true
-                }
-
-                MotionEvent.ACTION_CANCEL -> {
-                    button.setColorFilter(
-                        ContextCompat.getColor(this, R.color.white),
-                        PorterDuff.Mode.SRC_IN
-                    )
-                    v.isPressed = false
-                    true
-                }
-
-                else -> false
+            // Handle navigation
+            when (button.id) {
+                R.id.nav_back -> if (myWebView.canGoBack()) myWebView.goBack() else finish()
+                R.id.nav_home -> myWebView.loadUrl(homeUrl)
+                R.id.nav_tutorial -> myWebView.loadUrl("https://www.orthocalculator.org/app-video-library")
             }
         }
     }
+
+
+    fun tintIconTemporarily(item: MenuItem, context: Context, highlightColor: Int, defaultColor: Int, delay: Long = 1000L) {
+        val wrapped = DrawableCompat.wrap(item.icon!!).mutate()
+        DrawableCompat.setTint(wrapped, ContextCompat.getColor(context, highlightColor))
+        item.icon = wrapped
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val reset = DrawableCompat.wrap(item.icon!!).mutate()
+            DrawableCompat.setTint(reset, ContextCompat.getColor(context, defaultColor))
+            item.icon = reset
+        }, delay)
+    }
+
+
 
     override fun onBackPressed() {
         if (myWebView.canGoBack()) {
